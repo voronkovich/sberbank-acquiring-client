9991a60e4add20dc429d86d167e9d8de597a4455
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2016-04-29 14:37:49 +0300

    Simplify clien's api

--- src/Client.php
+++ src/Client.php
@@ -26,14 +26,23 @@ class Client
      */
     private $httpClient;
 
+    /**
+     * Constructor.
+     *
+     * @param array $settings Client's settings
+     */
     public function __construct(array $settings)
     {
         if (isset($settings['userName'])) {
             $this->userName = $settings['userName'];
+        } else {
+            throw new \LogicException('UserName is required.');
         }
 
         if (isset($settings['password'])) {
             $this->password = $settings['password'];
+        } else {
+            throw new \LogicException('Password is required.');
         }
 
         if (isset($settings['apiUri'])) {
@@ -41,63 +50,20 @@ class Client
         }
 
         if (isset($settings['httpMethod'])) {
-            $this->setHttpMethod($settings['httpMethod']);
+            if ('GET' !== $settings['httpMethod'] && 'POST' !== $settings['httpMethod']) {
+                throw new \UnexpectedValueException('An HTTP method must be "GET" or "POST".');
+            }
+
+            $this->httpMethod = $settings['httpMethod'];
         }
 
         if (isset($settings['httpClient'])) {
-            $this->setHttpClient($settings['httpClient']);
+            if (!$settings instanceof HttpClientInterface) {
+                throw new \UnexpectedValueException('An HTTP client must implement HttpClientInterface.');
+            }
+
+            $this->httpClient = $settings['httpClient'];
         }
-    }
-
-    public function setUserName($userName)
-    {
-        $this->userName = $userName;
-
-        return $this;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function setApiUri($apiUri)
-    {
-        $this->apiUri = $apiUri;
-
-        return $this;
-    }
-
-    /**
-     * Set an HTTP method.
-     *
-     * @param string $httpMethod 'GET' or 'POST'
-     *
-     * @return $this
-     */
-    public function setHttpMethod($httpMethod)
-    {
-        if ('GET' !== $httpMethod && 'POST' !== $httpMethod) {
-            throw new \UnexpectedValueException('An HTTP method must be "GET" or "POST".');
-        }
-
-        $this->httpMethod = $httpMethod;
-
-        return $this;
-    }
-
-    /**
-     * Set an HTTP client.
-     *
-     * @param HttpClientInterface $httpClient An HTTP client instance
-     */
-    public function setHttpClient(HttpClientInterface $httpClient)
-    {
-        $this->httpClient = $httpClient;
-
-        return $this;
     }
 
     /**
@@ -106,23 +72,14 @@ class Client
      * @param string $action An action's name e.g. 'register.do'
      * @param array  $data   An actions's data
      *
-     * @throws \LogicException
      * @throws ActionException
      * @throws NetworkException
      *
-     * @return array Server's response
+     * @return array A server's response
      */
     public function execute($action, array $data = array())
     {
         $uri = $this->apiUri . $action;
-
-        if ('' === $this->userName) {
-            throw new \LogicException('UserName is required.');
-        }
-
-        if ('' === $this->password) {
-            throw new \LogicException('Password is required.');
-        }
 
         $data['userName'] = $this->userName;
         $data['password'] = $this->password;
@@ -143,27 +100,22 @@ class Client
         return $response;
     }
 
+    /**
+     * Get an HTTP client.
+     *
+     * @return HttpClientInterface
+     */
     private function getHttpClient()
     {
         if (null === $this->httpClient) {
-            $this->httpClient = $this->createDefaultHttpClient();
+            $this->httpClient = new CurlClient(array(
+                \CURLOPT_VERBOSE => false,
+                \CURLOPT_SSL_VERIFYHOST => false,
+                \CURLOPT_SSL_VERIFYPEER => false,
+            ));
         }
 
         return $this->httpClient;
-    }
-
-    /**
-     * Create a default HTTP client.
-     *
-     * @return HttpClientInterface Client's instance
-     */
-    private function createDefaultHttpClient()
-    {
-        return new CurlClient(array(
-            \CURLOPT_VERBOSE => false,
-            \CURLOPT_SSL_VERIFYHOST => false,
-            \CURLOPT_SSL_VERIFYPEER => false,
-        ));
     }
 
     /**

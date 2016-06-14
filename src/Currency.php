e00732904065033ce6518566a08ec6f5d14ad191
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2016-06-14 19:02:02 +0300

    Add helper class with currencies codes

---
+++ src/Currency.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Voronkovich\SberbankAcquiring;
+
+/**
+ * Helper class with currencies codes (ISO 4217).
+ *
+ * @author Oleg Voronkovich <oleg-voronkovich@yandex.ru>
+ * @see https://en.wikipedia.org/wiki/ISO_4217
+ */
+class Currency
+{
+    const EUR = 978;
+    const RUB = 643;
+    const UAH = 980;
+    const USD = 840;
+}

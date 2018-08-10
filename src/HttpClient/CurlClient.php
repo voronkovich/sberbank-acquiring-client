371b5b9be35a90458a5fff0d98bd8d6c7e1170ac
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2018-08-10 18:12:00 +0300

    Refactor curl client

--- src/HttpClient/CurlClient.php
+++ src/HttpClient/CurlClient.php
@@ -13,23 +13,30 @@ use Voronkovich\SberbankAcquiring\Exception\NetworkException;
  */
 class CurlClient implements HttpClientInterface
 {
+    /**
+     * @var resource
+     */
     private $curl;
+
+    /**
+     * @var array
+     */
     private $curlOptions = [];
 
     public function __construct(array $curlOptions)
     {
-        if (!extension_loaded('curl')) {
+        if (!\extension_loaded('curl')) {
             throw new \RuntimeException('Curl extension is not loaded.');
         }
 
         $this->curlOptions = $curlOptions;
     }
 
-    private function getCurl()
+    private function getCurl(): resource
     {
         if (null === $this->curl) {
-            $this->curl = curl_init();
-            curl_setopt_array($this->curl, $this->curlOptions);
+            $this->curl = \curl_init();
+            \curl_setopt_array($this->curl, $this->curlOptions);
         }
 
         return $this->curl;
@@ -37,7 +44,7 @@ class CurlClient implements HttpClientInterface
 
     public function request(string $uri, string $method = 'GET', array $headers = [], array $data = []): array
     {
-        $data = http_build_query($data, '', '&');
+        $data = \http_build_query($data, '', '&');
 
         if ('GET' === $method) {
             $curlOptions[\CURLOPT_HTTPGET] = true;
@@ -47,25 +54,25 @@ class CurlClient implements HttpClientInterface
             $curlOptions[\CURLOPT_URL] = $uri;
             $curlOptions[\CURLOPT_POSTFIELDS] = $data;
         } else {
-            throw new \DomainException(sprintf('An HTTP method "%s" is not supported. Use "GET" or "POST".', $method));
+            throw new \InvalidArgumentException(\sprintf('An HTTP method "%s" is not supported. Use "GET" or "POST".', $method));
         }
 
         $curlOptions[\CURLOPT_HTTPHEADER] = $headers;
         $curlOptions[\CURLOPT_RETURNTRANSFER] = true;
 
         $curl = $this->getCurl();
-        curl_setopt_array($curl, $curlOptions);
+        \curl_setopt_array($curl, $curlOptions);
 
-        $response = curl_exec($curl);
+        $response = \curl_exec($curl);
 
         if (false === $response) {
-            $error = curl_error($curl);
-            $errorCode = curl_errno($curl);
+            $error = \curl_error($curl);
+            $errorCode = \curl_errno($curl);
 
             throw new NetworkException('Curl error: ' . $error, $errorCode);
         }
 
-        $httpCode = curl_getinfo($this->curl, \CURLINFO_HTTP_CODE);
+        $httpCode = \curl_getinfo($this->curl, \CURLINFO_HTTP_CODE);
 
         return [$httpCode, $response];
     }
@@ -73,7 +80,7 @@ class CurlClient implements HttpClientInterface
     public function __destruct()
     {
         if (null !== $this->curl) {
-            curl_close($this->curl);
+            \curl_close($this->curl);
         }
     }
 }
